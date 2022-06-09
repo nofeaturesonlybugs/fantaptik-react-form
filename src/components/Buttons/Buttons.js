8f7098c9eee88f33f622c68c378ae68c35581bc9
Robert Bredlau <Robert.Bredlau@broadlux.com>
2022-06-09 16:24:58 -0700

    Add Buttons.Save and Form.Buttons.Save

--- src/components/Buttons/Buttons.js
+++ src/components/Buttons/Buttons.js
@@ -1,75 +1,67 @@
-import React from 'react';
+import React from 'react'
 
-import { merge, Button } from '@fantaptik/react-material';
+import { merge, Button } from '@fantaptik/react-material'
 
-import icons from '../../conf/icons';
+import icons from '../../conf/icons'
 
-const Buttons = ( { children, ...props } ) => {
-    children = React.Children.map( children, child => {
-        return React.cloneElement( child, { ...props, ...child.props } );
-    } );
-    return <>{children}</>;
+const Buttons = ({ children, ...props }) => {
+	children = React.Children.map(children, (child) => {
+		return React.cloneElement(child, { ...props, ...child.props })
+	})
+	return <>{children}</>
 }
 
-const Cancel = ( { className, ...props } ) => {
-    className = merge`${className} cancel`;
-    //
-    return (
-        <Button icon={icons.cancel} label="Cancel" {...props} className={className} />
-    );
+const Cancel = ({ className, ...props }) => {
+	className = merge`${className} cancel`
+	//
+	return <Button icon={icons.cancel} label='Cancel' {...props} className={className} />
 }
 
-const Reset = ( { className, ...props } ) => {
-    className = merge`${className} reset`;
-    //
-    return (
-        <Button icon={icons.reset} label="Reset" {...props} className={className} />
-    );
+const Reset = ({ className, ...props }) => {
+	className = merge`${className} reset`
+	//
+	return <Button icon={icons.reset} label='Reset' {...props} className={className} />
 }
 
-const FullReset = ( { className, ...props } ) => {
-    className = merge`${className} full-reset`;
-    //
-    return (
-        <Button icon={icons.reset} label="Reset" {...props} className={className} />
-    );
+const FullReset = ({ className, ...props }) => {
+	className = merge`${className} full-reset`
+	//
+	return <Button icon={icons.reset} label='Reset' {...props} className={className} />
 }
 
-const Submit = ( { className, ...props } ) => {
-    className = merge`${className} submit`;
-    return (
-        <Button icon={icons.submit} label="Submit" {...props} className={className} />
-    );
+const Submit = ({ className, ...props }) => {
+	className = merge`${className} submit`
+	return <Button icon={icons.submit} label='Submit' {...props} className={className} />
 }
 
-const Create = ( { className, ...props } ) => {
-    className = merge`${className} create`;
-    return (
-        <Button icon={icons.create} label="Create" {...props} className={className} />
-    );
+const Save = ({ className, ...props }) => {
+	className = merge`${className} save`
+	return <Button icon={icons.save} label='Save' {...props} className={className} />
 }
 
-const Edit = ( { className, ...props } ) => {
-    className = merge`${className} edit`;
-    return (
-        <Button icon={icons.edit} label="Edit" {...props} className={className} />
-    );
+const Create = ({ className, ...props }) => {
+	className = merge`${className} create`
+	return <Button icon={icons.create} label='Create' {...props} className={className} />
 }
 
-const Delete = ( { className, ...props } ) => {
-    className = merge`${className} delete`;
-    return (
-        <Button icon={icons.delete} label="Delete" {...props} className={className} />
-    );
+const Edit = ({ className, ...props }) => {
+	className = merge`${className} edit`
+	return <Button icon={icons.edit} label='Edit' {...props} className={className} />
 }
 
-Buttons.Cancel = Cancel;
-Buttons.FullReset = FullReset;
-Buttons.Reset = Reset;
-Buttons.Submit = Submit;
+const Delete = ({ className, ...props }) => {
+	className = merge`${className} delete`
+	return <Button icon={icons.delete} label='Delete' {...props} className={className} />
+}
 
-Buttons.Create = Create;
-Buttons.Edit = Edit;
-Buttons.Delete = Delete;
+Buttons.Cancel = Cancel
+Buttons.FullReset = FullReset
+Buttons.Reset = Reset
+Buttons.Submit = Submit
+Buttons.Save = Save
 
-export default Buttons;
+Buttons.Create = Create
+Buttons.Edit = Edit
+Buttons.Delete = Delete
+
+export default Buttons
