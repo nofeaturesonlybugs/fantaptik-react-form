070259387b35ccee2ffdf2b72503aec681db506d
Nofeatures Onlybugs <nofeaturesonlybugs@gmail.com>
2022-06-09 12:19:02 -0700

    Remove missing components from src/index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 export { default as Buttons } from './components/Buttons/Buttons'
-export { MakeCancelButton } from './components/Buttons/Buttons'
-export { MakeResetButton } from './components/Buttons/Buttons'
-export { MakeSubmitButton } from './components/Buttons/Buttons'
 
 export { default as Form } from './components/Form/Form'
-
-export { default as ChangePassword } from './forms/ChangePassword/ChangePassword'
-export { default as Login } from './forms/Login/Login'
 
 export { default as Promises } from './lib/Promises'
 
