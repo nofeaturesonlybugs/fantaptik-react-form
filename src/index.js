af02d054facd27fac7248865fc9ddd0040d514ee
Nofeatures Onlybugs <nofeaturesonlybugs@gmail.com>
2022-06-09 10:09:09 -0700

    Reformat some js files

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-export { default as Buttons } from './components/Buttons/Buttons';
-export { MakeCancelButton } from './components/Buttons/Buttons';
-export { MakeResetButton } from './components/Buttons/Buttons';
-export { MakeSubmitButton } from './components/Buttons/Buttons';
+export { default as Buttons } from './components/Buttons/Buttons'
+export { MakeCancelButton } from './components/Buttons/Buttons'
+export { MakeResetButton } from './components/Buttons/Buttons'
+export { MakeSubmitButton } from './components/Buttons/Buttons'
 
-export { default as Form } from './components/Form/Form';
+export { default as Form } from './components/Form/Form'
 
-export { default as ChangePassword } from './forms/ChangePassword/ChangePassword';
-export { default as Login } from './forms/Login/Login';
+export { default as ChangePassword } from './forms/ChangePassword/ChangePassword'
+export { default as Login } from './forms/Login/Login'
 
-export { default as conf } from './conf/conf';
+export { default as Promises } from './lib/Promises'
 
-export { default as Promises } from './lib/Promises';
-
-export { default as lib } from './lib/lib';
+export { default as lib } from './lib/lib'
